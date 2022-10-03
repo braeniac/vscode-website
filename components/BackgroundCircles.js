5c775c47456294a4de926796a00ca63d821be309
Maninder Singh <maninder.singh.live@gmail.com>
2022-10-03 17:19:56 -0400

    circle animation on homepage complete

---
+++ components/BackgroundCircles.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { motion } from 'framer-motion'; 
+
+function BackgroundCircles() {
+  return (
+    <motion.div 
+      className='hidden md:relative md:flex justify-center items-center ml-5'
+      intial={{
+        opacity:0
+      }}
+      animate={{
+        scale:[1,2,2,3,1],
+        opacity:[0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+        borderRadius: ["20%", "20%", "50%", "80%", "20%"],
+      }}
+      transition={{
+        duration:2.5
+      }}
+    >
+      <div className='absolute border border-[#333] rounded-full h-[200px] w-[200px] animate-ping mt-36' />
+      <div className='absolute border border-[#333] rounded-full h-[300px] w-[300px] animate-ping mt-36' />
+      <div className='absolute border border-[#333] rounded-full h-[500px] w-[500px] animate-ping mt-36' />
+      <div className='absolute border border-[#f9826c] rounded-full h-[650px] w-[650px] animate-pulse mt-36'/>
+      <div className='absolute border-8 border-[#333] rounded-full h-[800px] w-[800px] animate-ping mt-36' />
+    </motion.div>
+  )
+}
+
+export default BackgroundCircles
--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,33 @@ import { useRouter } from 'next/router';
 import { motion } from "framer-motion"; 
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 
+import BackgroundCircles from '../components/BackgroundCircles'; 
+
 export default function Home() {
 
   const router = useRouter(); 
-  const [text, count] = useTypewriter({
+  const [text, ] = useTypewriter({
     words: [
       "Full-Stack Developer", 
       "Guy-Who-Loves-Coffee.tsx", 
-      "<ButLovesToCodeMode />"
+      "<ButLovesToCodeMore />"
     ],
-    loop: true,
-    delaySpeed: 2000
+    loop: 3,
+    delaySpeed: 2000,
   }); 
 
   return (
-    <div className='flex bg-[#24292e] h-screen overflow-y-auto text-white'> 
+    <div className='flex bg-[#24292e] h-screen overflow-hidden text-white pt-10'> 
       <div className='flex-col  self-center mt-[-150px] text-justify md:text-left pl-10'>
+        <BackgroundCircles />
         <h1 className='text-6xl pb-3'>Maninder Singh</h1>
-
         <h1>
-          <span className='text-2xl md:text-3xl'>{text}</span>
-          <Cursor cursorColor='#f9826c'/>
+          <span className='text-xl md:text-2xl'>{text}</span>
+          <Cursor cursorColor='#f9826c' cursorStyle='_' />
         </h1>
-
         <div className='pt-5 space-x-4 font-bold'>
           <motion.button 
-            className='bg-[#f9826c] border-2 py-1 px-2 border-[#f9826c]'
+            className='bg-[#f9826c] border-2 py-1 px-2 border-[#f9826c] relative'
             onClick={() => router.push('/Projects')}
             whileHover={{ scale: 1.2 }}
             onHoverStart={e => {}}
@@ -37,7 +38,7 @@ export default function Home() {
             <p>View Work</p>
           </motion.button>
           <motion.button 
-            className='bg-[#24292e] border-2 py-1 px-2 border-[#f9826c]'
+            className='bg-[#24292e] border-2 py-1 px-2 border-[#f9826c] relative'
             onClick={() => router.push('/Contact')}  
             whileHover={{ scale: 1.2 }}
             onHoverStart={e => {}}
